58b761a6eb10a83d68308837cfd5a432c0764b8e
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-03-10 15:09:38 -0300

    Create Booleana.py

---
+++ Booleana.py
@@ -0,0 +1,12 @@
+''' Exemplo de Variáveis Booleanas -> True // False '''
+
+tempo_definido = 15
+hora_de_dormir = 8
+
+print(hora_de_dormir > tempo_definido)
+
+comeu = True
+bebeu = False
+ambos = bebeu and comeu
+
+print(ambos)
