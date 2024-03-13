1c9de85ba4a65992cfde54664e9c1e2e5f478baa
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-03-13 10:48:42 -0300

    Add files via upload

---
+++ soma_int.py
@@ -0,0 +1,10 @@
+'''Escreva um programa que receba três números e imprima sua soma:'''
+
+
+numero_1 = int(input("Digitar o primeiro número a ser somado: "))
+numero_2 = int(input("Digitar o segundo número a ser somado: "))
+numero_3 = int(input("Digitar o terceiro número a ser somado: "))
+
+soma_total = numero_1 + numero_2 + numero_3
+
+print(f"A soma total é = {soma_total}")
