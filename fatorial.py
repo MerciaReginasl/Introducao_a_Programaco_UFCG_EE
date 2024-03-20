24be27e829beec446ae4735173d9aee819dfbe89
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-03-20 11:52:55 -0300

    Add files via upload

--- fatorial.py
+++ fatorial.py
@@ -1,7 +1,18 @@
-'''Calculo do fatorial de um número natural'''
+'''
 
-numero = int(input("Informe um número natural: "))
-fatorial = 1
-for fator in range (1, numero+1, 1):
-	fatorial *= fator
-print(f"O fatorial de {numero} é {fatorial}")
+Faça um programa que mostre a soma dos fatoriais de dois números naturais.
+O programa usará uma função fatorial que recebe um número natural e retorna 
+seu fatorial
+
+'''
+
+def fatorial(natural):
+	fat = 1
+	for fator in range (1, natural+1, 1):
+		fat *= fatorial
+	return fat
+	
+nat1 = int(input("Informe um número natural: "))
+nat2 = int(input("Informe um número natural: "))
+
+print(f"A soma dos fatorais de {nat1} e {nat2} é {fatorial(nat1)+fatorial(nat2)})
